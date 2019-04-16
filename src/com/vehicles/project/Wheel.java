167c234b2b2ea9b0412198d13c4455eda53768c0
Rayleighomega <>
2019-04-16 12:54:29 +0200

    Fase2 completada, faltaba comprobar el valor mínimo y máximo de las ruedas

--- src/com/vehicles/project/Wheel.java
+++ src/com/vehicles/project/Wheel.java
@@ -1,14 +1,44 @@
 package com.vehicles.project;
 
-import java.util.List;
-
-public class Wheel {
+public class Wheel
+{
 	private String brand;
 	private double diameter;
 
 	public Wheel(String brand, double diameter) {
 		this.brand = brand;
-		this.diameter = diameter;
+
+		try
+		{
+			if (diameter >= 0.4 && diameter < 4)
+			{
+				this.diameter = diameter;
+			}
+			else
+			{
+				throw new Exception();
+			}
+		}
+		catch (Exception e)
+		{
+			System.err.println("Diametro de la rueda incorrecto");
+			System.out.println(" ");
+			if (diameter < 0.4)
+			{
+				this.diameter = 0.4;
+				System.err.println("No puede ser menor que 0.4, establecido al valor mínimo");
+				System.out.println(" ");
+			}
+			else if (diameter > 4)
+			{
+				this.diameter = 4;
+				System.err.println("No puede ser mayor que 4, establecido al valor máximo");
+				System.out.println(" ");
+			}
+
+		}
+
+
 	}
 
 	public String getBrand()
