eb2b57852ce5d8a8ecd4eeb32d64f2665c4560cd
Rayleighomega <>
2019-04-16 13:08:44 +0200

    Fase 3 no completada

--- src/com/vehicles/project/Vehicle.java
+++ src/com/vehicles/project/Vehicle.java
@@ -67,4 +67,24 @@ public abstract class Vehicle {
 		return plateOk;
 	}
 
+	public void addWheels(List<Wheel> frontWheels, List<Wheel> backWheels) throws Exception {
+		addTwoWheels(frontWheels);
+		addTwoWheels(backWheels);
+	}
+
+	public void addTwoWheels(List<Wheel> wheels) throws Exception {
+		if (wheels.size() != 2)
+			throw new Exception();
+
+		Wheel rightWheel = wheels.get(0);
+		Wheel leftWheel = wheels.get(1);
+
+		/*if (!rightWheel.equals(leftWheel))
+			throw new Exception();
+		*/
+
+		this.wheels.add(leftWheel);
+		this.wheels.add(rightWheel);
+	}
+
 }
