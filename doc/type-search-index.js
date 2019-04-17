5622b1cdfa9254c842a8fc9ed6ba81299c7c4e51
Rayleighomega <>
2019-04-17 11:37:40 +0200

    Add javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.vehicles.project","l":"Bike"},{"p":"com.vehicles.project","l":"Car"},{"p":"com.vehicles.project","l":"Main"},{"p":"com.vehicles.project","l":"Vehicle"},{"p":"com.vehicles.project","l":"Wheel"}]
